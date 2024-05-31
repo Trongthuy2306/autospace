5a15eff1de26cd144287d1ed3e97e519fe7a6524
Trongthuy2306 <thuynguyen.23062002@gmail.com>
2024-05-31 16:11:25 +0700

    Validate script

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,5 +1,5 @@
-import { add } from "@autospace/sample-lib";
-import Image from "next/image";
+import { add } from '@autospace/sample-lib'
+import Image from 'next/image'
 
 export default function Home() {
   return <main>hello {add(33, 11)}</main>
